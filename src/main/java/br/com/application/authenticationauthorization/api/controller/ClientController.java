37343eace25bacaa5166c42d9e475bb4a207c286
taylorsantosoliveira@hotmail.com <Suco071952804$>
2022-04-01 17:27:52 -0300

    Spring Security - Autenticação formLogin.

--- src/main/java/br/com/application/authenticationauthorization/api/controller/ClientController.java
+++ src/main/java/br/com/application/authenticationauthorization/api/controller/ClientController.java
@@ -2,6 +2,7 @@ package br.com.application.authenticationauthorization.api.controller;
 
 import br.com.application.authenticationauthorization.domain.repository.ClientRepository;
 import br.com.application.authenticationauthorization.domain.model.Client;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class ClientController {
     private final ClientRepository clientRepository;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Client> listAll() {
         return clientRepository.findAll();
     }
--- src/main/java/br/com/application/authenticationauthorization/core/WebSecurityConfig.java
+++ src/main/java/br/com/application/authenticationauthorization/core/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package br.com.application.authenticationauthorization.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,12 +18,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
             .and()
-            .formLogin();
-//                .loginPage("/login")
-//                .permitAll()
-//            .and()
-//                .logout()
-//                .logoutSuccessUrl("/login?logout")
-//                .permitAll();
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("123"));
     }
 }
